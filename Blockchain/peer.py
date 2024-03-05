0c788387754b6c0b1f9227c196c3ee161d690c48
JorisTDP <joriselfferich@gmail.com>
2024-03-05 14:09:20 +0100

    blockchain hotfix

--- Blockchain/peer.py
+++ Blockchain/peer.py
@@ -34,7 +34,10 @@ def get_chain():
     chain = []
     #create a new chain from our blockchain
     for block in blockchain.chain:
-        chain.append(block.to_dict())
+        if isinstance(block, dict):
+            chain.append(block)
+        else:
+            chain.append(block.to_dict())
     print(chain)
     print("Chain Len: {0}".format(len(chain)))
     return json.dumps({"length" : len(chain), "chain" : chain})
