381a59a772da1241f0983b191eb9e69fa53a87a2
JorisTDP <joriselfferich@gmail.com>
2024-04-10 14:27:25 +0200

    Succesfull data transmit LoRaWAN and schedule downlink test

--- Blockchain/test.py
+++ Blockchain/test.py
@@ -1,23 +1,33 @@
-import base64
+import requests
 
-# Read the contents of the file as bytes
-with open('test.pdf', 'rb') as file:
-    file_data = file.read()
+# Define the authorization token
+authorization_token = "Bearer NNSXS.Z5XAZZ7T7PTICIRYEVJBJEODFKLEGCV75BUOMWY.KZG7VOHDCL4HXKA3F3V2XJQDMOWKV2HUM4UMQNZYZSD3SUDFSQJA"
 
-# Encode the binary data as Base64
-file_data_base64 = base64.b64encode(file_data)
+# Define the payload data
+payload_data = {
+    "downlinks": [{
+        "frm_payload": "vu8=",
+        "f_port": 15,
+        "priority": "NORMAL"
+    }]
+}
 
-# Convert the Base64-encoded bytes to a string
-file_data_base64_string = file_data_base64.decode('utf-8')
+# Define the headers
+headers = {
+    "Authorization": authorization_token,
+    "Content-Type": "application/json",
+    "User-Agent": "DIHM-LoRa/1.0"
+}
 
-# Print the Base64-encoded string
-print(file_data_base64_string)
+# Define the URL
+url = "https://eu1.cloud.thethings.network/api/v3/as/applications/dihm-module/webhooks/blockchain-server/devices/eui-70b3d57ed0066206/down/push"
 
-# Decode the Base64-encoded string back to binary
-decoded_data = base64.b64decode(file_data_base64_string)
-print(decoded_data)
-with open('test1.pdf', 'wb') as file:
-    file.write(decoded_data)
+# Make the POST request
+response = requests.post(url, json=payload_data, headers=headers)
 
-# Verify that the decoded data matches the original data
-print(decoded_data == file_data)  # Should print True if decoding is successful
+# Check if the request was successful
+if response.status_code == 200:
+    print("Downlink message scheduled successfully!")
+else:
+    print("Failed to schedule downlink message. Status code:", response.status_code)
+
