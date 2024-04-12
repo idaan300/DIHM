aa3009cc66ab97371b705f71e1d536ee24325b64
JorisTDP <joriselfferich@gmail.com>
2024-04-12 14:24:50 +0200

    arduino code lora ready

--- arduinocode/DIHM-Module/DIHM-Module.ino
+++ arduinocode/DIHM-Module/DIHM-Module.ino
@@ -15,6 +15,8 @@ bool deviceConnected = false;
 bool dataSent = false;
 bool oldDeviceConnected = false;
 uint32_t value = 0;
+const char *uplinkMessage = "Sending...";
+uint8_t downlinkData[64];
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -60,13 +62,20 @@ void setup() {
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
+  // Build payload byte array
 }
 
 void loop() {
   if (deviceConnected) {
-//        int state = node.sendReceive(uplinkPayload, sizeof(uplinkPayload)); //uplink and downlink same function    
-//        debug((state != RADIOLIB_LORAWAN_NO_DOWNLINK) && (state != RADIOLIB_ERR_NONE), F("Error in sendReceive"), state, false);
-        sendData(); // sendData(lora_downlink);
+        uint8_t byteArray[] = {
+    'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ',
+    't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', '\0'
+};      sendLora(); //Example byte array
+        size_t arrayLength = sizeof(downlinkData) / sizeof(downlinkData[0]); 
+        unsigned char unsignedCharArray[arrayLength];
+        byteArrayToUnsignedCharArray(downlinkData, unsignedCharArray, arrayLength);
+        
+        sendData(unsignedCharArray); // sendData(lora_downlink);
         delay(1000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
     // disconnecting
@@ -85,6 +94,12 @@ void loop() {
     }
 }
 
+void sendLora(){
+    size_t downlinkSize = 0;
+    int state = node.sendReceive(uplinkMessage, 1, downlinkData, &downlinkSize, true); //uplink and downlink same function    
+    debug((state != RADIOLIB_LORAWAN_NO_DOWNLINK) && (state != RADIOLIB_ERR_NONE), F("Error in sendReceive"), state, false);
+}
+
 void stringToUnsignedCharArray(const String &input, unsigned char output[], size_t maxLength) {
   size_t length = input.length();
   if (length > maxLength) {
@@ -95,15 +110,23 @@ void stringToUnsignedCharArray(const String &input, unsigned char output[], size
   }
 }
 
-void sendData(){
+void byteArrayToUnsignedCharArray(uint8_t *data, unsigned char *result, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        result[i] = static_cast<unsigned char>(data[i]);
+    }
+}
+
+void sendData(unsigned char inp[]){
     //fullstring = given bij LoRa downlink and should be decode from base64
-    String fullString = "fCBVc2VyOiBKb3JpcywgRGVzY3I6IFVwbG9hZCAxLCBGaWxlOiBBZnN0dWRlZXJPcGRyYWNodF9TY3JpcHRpZS5wZGYsIFRpbWU6IDIwMjQtMDQtMDIgMTQ6MTY6MTQgfCBVc2VyOiBKb3JpcywgRGVzY3I6IElobSB2ZXJzaWUgMiwNCg0KLi4uIA0KDQouLi4sIEZpbGU6IFJFQURNRS5tZCwgVGltZTogMjAyNC0wNC0wMyAxMjoxNToxMyA=";
+//    String fullString = "fCBVc2VyOiBKb3JpcywgRGVzY3I6IFVwbG9hZCAxLCBGaWxlOiBBZnN0dWRlZXJPcGRyYWNodF9TY3JpcHRpZS5wZGYsIFRpbWU6IDIwMjQtMDQtMDIgMTQ6MTY6MTQgfCBVc2VyOiBKb3JpcywgRGVzY3I6IElobSB2ZXJzaWUgMiwNCg0KLi4uIA0KDQouLi4sIEZpbGU6IFJFQURNRS5tZCwgVGltZTogMjAyNC0wNC0wMyAxMjoxNToxMyA=";
+//    int len = fullString.length();
+//    unsigned char charred[1000];
+//    stringToUnsignedCharArray(fullString,charred,1000); 
+//    unsigned char decoded_text[1000];
+//    int decoded_length = decode_base64(inp,decoded_text);
+//    String fullString = (char *)decoded_text;
+    String fullString = (char *)inp;
     int len = fullString.length();
-    unsigned char charred[1000];
-    stringToUnsignedCharArray(fullString,charred,1000); 
-    unsigned char decoded_text[1000];
-    int decoded_length = decode_base64(charred,decoded_text);
-    fullString = (char *)decoded_text;
     int chunkSize = 20;
     if(dataSent == false){
       for (int i = 0; i < len; i += chunkSize) {
