f5bc01c652f1cb15affe948316ad80919b92cf0a
JorisTDP <joriselfferich@gmail.com>
2024-04-25 12:21:39 +0200

    stuur maar een pakker van 242 bytes

--- Blockchain/peer.py
+++ Blockchain/peer.py
@@ -59,13 +59,13 @@ def info():
     inf = blockchain.getInfo(chain)
     data_bytes = inf.encode('utf-8')
     encoded_data = base64.b64encode(data_bytes).decode('utf-8')
-    # chunk_size = 242
-    # chunks = [encoded_data[i:i+chunk_size] for i in range(0, len(encoded_data), chunk_size)]
+    chunk_size = 242
+    chunks = [encoded_data[0:chunk_size]]# for i in range(0, len(encoded_data), chunk_size)]
 
-    # for chunk in chunks:
+    #for chunk in chunks:
     payload_data = {
         "downlinks": [{
-            "frm_payload": encoded_data,#chunk,
+            "frm_payload": chunks,
             "f_port": 15,
             "priority": "NORMAL"
         }]
